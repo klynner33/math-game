275d028809765d9e589fdcd3354ac6822fea7e5b
klynner33 <113389068+klynner33@users.noreply.github.com>
2024-01-09 22:50:19 -0600

    Added api template

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ checkAnswer() {
     this.resultElement.textContent = `You are correct!`;
   }else {
     this.resultElement.textContent = `Incorrect. The correct answer is ${this.correctAnswer}.`;
-}
+  }
 }
 }
 
@@ -47,3 +47,31 @@ document.querySelector('#answer').addEventListener('keyup', function(event) {
     document.querySelector('#check-answer').click();
   }
 })
+
+
+//Fix API to be functional in this code
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+  const url = `www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        if(data.media_type === "image"){
+          document.querySelector('img').src = data.hdurl
+        }else if(data.media_type === 'video'){
+          document.querySelector('iframe').src = data.url
+        }else{
+          alert('Media Not Supported - Contact NASA IMMEDIATLY')
+        }
+       
+        document.querySelector('h3').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
