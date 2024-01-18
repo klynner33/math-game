6b8053d9f892b44c20c778179da2d0e0be9aa8cb
klynner33 <113389068+klynner33@users.noreply.github.com>
2024-01-18 10:06:50 -0600

    Get fetch working inside the class and added a reset function

--- script.js
+++ script.js
@@ -1,91 +1,100 @@
-//Fix API to be functional in this code
-// document.querySelector('button').addEventListener('click', getFetch)
-
-function spriteFetch(){
-  // const choice = document.querySelector('input').value
-  // console.log(choice)
-  const url = `https://pokeapi.co/api/v2/pokemon/pikachu`
-
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data.sprites.front_default)
-
-        const imgUrls = [];
-        for(let i = 0; i < pokemonGame.num1; i++) {
-          imgUrls.push(data.sprites.front_default);
-        }
-
-        const spriteLeft = document.querySelector('#sprite-left')
-        for (let i = 0; i < imgUrls.length; i++) {
-          const img = document.createElement('img');
-          img.src = imgUrls[i];
-          spriteLeft.appendChild(img);
-        }
-        
-        const imageUrls = [];
-        for(let i = 0; i < pokemonGame.num2; i++) {
-          imageUrls.push(data.sprites.front_default);
-        }
-
-        const spriteRight = document.querySelector('#sprite-right')
-        for (let i = 0; i < imageUrls.length; i++) {
-          const img = document.createElement('img');
-          img.src = imageUrls[i];
-          spriteRight.appendChild(img);
-        }
-        
-       
-        // document.querySelector('h3').innerText = data.explanation
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
-
 class AdditionGame {
-constructor() {
-  this.num1Element = document.querySelector('#first-number');
-  this.num2Element = document.querySelector('#second-number');
-  this.answerElement = document.querySelector('#answer');
-  this.resultElement =document.querySelector('#result');
-}
-
-generateQuestion() {
-  this.num1 = Math.floor(Math.random() * 10);
-  this.num2 = Math.floor(Math.random() * 10);
-
-  this.num2Element.textContent = this.num2;
-  this.num1Element.textContent = this.num1;
-
-  this.answerElement.focus();
-  this.answerElement.value = '';
-  this.resultElement.textContent = '';
-}
-
-checkAnswer() {
-  this.correctAnswer = this.num1 + this.num2;
-  console.log(this.correctAnswer);
-  this.userAnswer = this.answerElement.value;
-  console.log(this.userAnswer);
-  if (Number(this.userAnswer) === Number(this.correctAnswer)) {
-    this.resultElement.textContent = `You are correct!`;
-  }else {
-    this.resultElement.textContent = `Incorrect. The correct answer is ${this.correctAnswer}.`;
+  constructor() {
+    this.num1Element = document.querySelector('#first-number');
+    this.num2Element = document.querySelector('#second-number');
+    this.answerElement = document.querySelector('#answer');
+    this.resultElement = document.querySelector('#result');
   }
-}
+
+  generateQuestion() {
+    this.num1 = Math.floor(Math.random() * 10);
+    this.num2 = Math.floor(Math.random() * 10);
+
+    this.num2Element.textContent = this.num2;
+    this.num1Element.textContent = this.num1;
+
+    this.answerElement.focus();
+    this.answerElement.value = '';
+    this.resultElement.textContent = '';
+
+    this.spriteLeft = document.querySelector('#sprite-left');
+    this.spriteRight = document.querySelector('#sprite-right');
+    
+  }
+  
+
+  spriteFetch(){
+    // const choice = document.querySelector('input').value
+    // console.log(choice)
+    const url = `https://pokeapi.co/api/v2/pokemon/pikachu`
+  
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          console.log(data.sprites.front_default)
+  
+          const img1Urls = [];
+          for(let i = 0; i < this.num1; i++) {
+            img1Urls.push(data.sprites.front_default);
+          }
+  
+          for (let i = 0; i < img1Urls.length; i++) {
+            const img = document.createElement('img');
+            img.src = img1Urls[i];
+            this.spriteLeft.appendChild(img);
+          }
+          
+          const img2Urls = [];
+          for(let i = 0; i < this.num2; i++) {
+            img2Urls.push(data.sprites.front_default);
+          }
+  
+          
+          for (let i = 0; i < img2Urls.length; i++) {
+            const img = document.createElement('img');
+            img.src = img2Urls[i];
+            this.spriteRight.appendChild(img);
+          }
+          
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+  }
+
+  checkAnswer() {
+    this.correctAnswer = this.num1 + this.num2;
+    console.log(this.correctAnswer);
+    this.userAnswer = this.answerElement.value;
+    console.log(this.userAnswer);
+    if (Number(this.userAnswer) === Number(this.correctAnswer)) {
+    this.resultElement.textContent = `You are correct!
+    You earn a character!`;
+    }else {
+    this.resultElement.textContent = `Incorrect. The correct answer is ${this.correctAnswer}.`;
+    }
+  }
+
+  reset() {
+   this.spriteLeft.innerHTML = '';
+   this.spriteRight.innerHTML = '';
+  }
 }
 
 const pokemonGame = new AdditionGame();
 
 document.querySelector('#go').addEventListener('click', function() {
+  // pokemonGame.reset();
+  
   pokemonGame.generateQuestion();
-  spriteFetch();
+  pokemonGame.spriteFetch();
   console.log(pokemonGame.num1)
 })
 
 document.querySelector('#check-answer').addEventListener('click', function() {
   pokemonGame.checkAnswer();
+  pokemonGame.reset();
+
   // getFetch();
 })
 
