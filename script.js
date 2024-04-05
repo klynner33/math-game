2aeae5fa425fc4ca8a279037174fadfd994be908
klynner33 <113389068+klynner33@users.noreply.github.com>
2024-04-05 10:19:41 -0500

    savepoint

--- script.js
+++ script.js
@@ -1,97 +1,289 @@
-//Fix API to be functional in this code
-// document.querySelector('button').addEventListener('click', getFetch)
+class MathGame {
+  constructor() {
+    this.equationElement = document.querySelector('#equation');
+    this.answerElement = document.querySelector('#answer');
+    this.resultElement = document.querySelector('#result');
+    this.spriteLeft = document.querySelector('#sprite-left');
+    this.spriteRight = document.querySelector('#sprite-right');
+  }
+  
+  spriteFetch(){
+    // const choice = document.querySelector('input').value
+    // console.log(choice)
+    const url = `https://pokeapi.co/api/v2/pokemon/pikachu`
+  
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          console.log(data.sprites.front_default)
+  
+          const img1Urls = [];
+          for(let i = 0; i < this.num1; i++) {
+            img1Urls.push(data.sprites.front_default);
+          }
+  
+          for (let i = 0; i < img1Urls.length; i++) {
+            const img = document.createElement('img');
+            img.src = img1Urls[i];
+            this.spriteLeft.appendChild(img);
+          }
+          
+          const img2Urls = [];
+          for(let i = 0; i < this.num2; i++) {
+            img2Urls.push(data.sprites.front_default);
+          }
+  
+          for (let i = 0; i < img2Urls.length; i++) {
+            const img = document.createElement('img');
+            img.src = img2Urls[i];
+            this.spriteRight.appendChild(img);
+          }
+          
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+  }
 
-function spriteFetch(){
-  // const choice = document.querySelector('input').value
-  // console.log(choice)
-  const url = `https://pokeapi.co/api/v2/pokemon/pikachu`
-
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data.sprites.front_default)
-
-        const imgUrls = [];
-        for(let i = 0; i < pokemonGame.num1; i++) {
-          imgUrls.push(data.sprites.front_default);
-        }
-
-        const spriteLeft = document.querySelector('#sprite-left')
-        for (let i = 0; i < imgUrls.length; i++) {
-          const img = document.createElement('img');
-          img.src = imgUrls[i];
-          spriteLeft.appendChild(img);
-        }
-        
-        const imageUrls = [];
-        for(let i = 0; i < pokemonGame.num2; i++) {
-          imageUrls.push(data.sprites.front_default);
-        }
-
-        const spriteRight = document.querySelector('#sprite-right')
-        for (let i = 0; i < imageUrls.length; i++) {
-          const img = document.createElement('img');
-          img.src = imageUrls[i];
-          spriteRight.appendChild(img);
-        }
-        
-       
-        // document.querySelector('h3').innerText = data.explanation
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
-
-class AdditionGame {
-constructor() {
-  this.num1Element = document.querySelector('#first-number');
-  this.num2Element = document.querySelector('#second-number');
-  this.answerElement = document.querySelector('#answer');
-  this.resultElement =document.querySelector('#result');
-}
-
-generateQuestion() {
-  this.num1 = Math.floor(Math.random() * 10);
-  this.num2 = Math.floor(Math.random() * 10);
-
-  this.num2Element.textContent = this.num2;
-  this.num1Element.textContent = this.num1;
-
-  this.answerElement.focus();
-  this.answerElement.value = '';
-  this.resultElement.textContent = '';
-}
-
-checkAnswer() {
-  this.correctAnswer = this.num1 + this.num2;
-  console.log(this.correctAnswer);
-  this.userAnswer = this.answerElement.value;
-  console.log(this.userAnswer);
-  if (Number(this.userAnswer) === Number(this.correctAnswer)) {
-    this.resultElement.textContent = `You are correct!`;
-  }else {
-    this.resultElement.textContent = `Incorrect. The correct answer is ${this.correctAnswer}.`;
+  reset() {
+   this.spriteLeft.innerHTML = '';
+   this.spriteRight.innerHTML = '';
   }
 }
+
+// ADDITION GAME
+
+class AdditionMathGame extends MathGame {
+  generateQuestion() {
+    this.reset();
+
+    this.num1 = Math.floor(Math.random() * 11);
+    this.num2 = Math.floor(Math.random() * 11);
+  
+    this.equationElement.textContent = `${this.num1} + ${this.num2} = `;
+
+    this.answerElement.focus();
+    this.answerElement.value = '';
+    this.resultElement.textContent = '';
+
+    this.spriteFetch();
+    
+    document.querySelector('#check-answer').addEventListener('click', function() {
+      game.checkAnswer();
+    })
+    
+    document.querySelector('#answer').addEventListener('keyup', function(event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        document.querySelector('#check-answer').click();
+      }
+    })
+  }
+
+  checkAnswer() {
+      let userAnswer = parseInt(this.answerElement.value);
+      let correctAnswer = this.num1 + this.num2;
+
+      if (userAnswer === correctAnswer) {
+          this.resultElement.textContent = `You are correct!
+          You earn a character!`;
+      } else {
+          this.resultElement.textContent = `Incorrect. The correct answer is ${correctAnswer}.`;
+      }
+  }
 }
 
-const pokemonGame = new AdditionGame();
 
-document.querySelector('#go').addEventListener('click', function() {
-  pokemonGame.generateQuestion();
-  spriteFetch();
-  console.log(pokemonGame.num1)
-})
+//SUBTRACTION GAME
 
-document.querySelector('#check-answer').addEventListener('click', function() {
-  pokemonGame.checkAnswer();
-  // getFetch();
-})
+class SubtractionMathGame extends MathGame {
+  generateQuestion() {
+    this.reset();
 
-document.querySelector('#answer').addEventListener('keyup', function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    document.querySelector('#check-answer').click();
+    this.num1 = Math.floor(Math.random() * 11);
+    this.num2 = Math.floor(Math.random() * 11);
+
+    if(this.num1 > this.num2) {
+    this.equationElement.textContent = `${this.num1} - ${this.num2} = `;
+    
+    this.answerElement.focus();
+    this.answerElement.value = '';
+    this.resultElement.textContent = '';
+
+    this.spriteFetch();
+    
+    document.querySelector('#check-answer').addEventListener('click', function() {
+      game.checkAnswer();
+    })
+    
+    document.querySelector('#answer').addEventListener('keyup', function(event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        document.querySelector('#check-answer').click();
+      }
+    })
+    } else {
+      this.generateQuestion();
+    }
   }
+
+  checkAnswer() {
+      let userAnswer = parseInt(this.answerElement.value);
+      let correctAnswer = this.num1 - this.num2;
+
+      if (userAnswer === correctAnswer) {
+          this.resultElement.textContent = `You are correct!
+          You earn a character!`;
+      } else {
+          this.resultElement.textContent = `Incorrect. The correct answer is ${correctAnswer}.`;
+      }
+  }
+}
+
+// MULTIPLICATION GAME
+
+class MultiplicationMathGame extends MathGame {
+  generateQuestion() {
+    this.reset();
+
+    this.num1 = Math.floor(Math.random() * 13);
+    this.num2 = Math.floor(Math.random() * 13);
+  
+    this.equationElement.textContent = `${this.num1} x ${this.num2} = `;
+
+    this.answerElement.focus();
+    this.answerElement.value = '';
+    this.resultElement.textContent = '';
+    
+    document.querySelector('#check-answer').addEventListener('click', function() {
+      game.checkAnswer();
+    })
+    
+    document.querySelector('#answer').addEventListener('keyup', function(event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        document.querySelector('#check-answer').click();
+      }
+    })
+  }
+
+  checkAnswer() {
+      let userAnswer = parseInt(this.answerElement.value);
+      let correctAnswer = this.num1 * this.num2;
+
+      if (userAnswer === correctAnswer) {
+          this.resultElement.textContent = `You are correct!
+          You earn a character!`;
+      } else {
+          this.resultElement.textContent = `Incorrect. The correct answer is ${correctAnswer}.`;
+      }
+  }
+}
+
+
+//DIVISION GAME
+
+class DivisionMathGame extends MathGame {
+  generateQuestion() {
+    this.reset();
+
+    this.num1 = Math.floor(Math.random() * 145);
+    this.num2 = Math.floor(Math.random() * 13);
+
+    if (this.num1 > this.num2 && this.num1 % this.num2 === 0) {
+      if (this.num1 <= 100 || (this.num2 === 11 || this.num2 === 12)) {
+        this.equationElement.textContent = `${this.num1} / ${this.num2} = `;
+      } else {
+        this.generateQuestion();
+      }
+    } else {
+      this.generateQuestion();
+    }
+
+    this.answerElement.focus();
+    this.answerElement.value = '';
+    this.resultElement.textContent = '';
+    
+    document.querySelector('#check-answer').addEventListener('click', function() {
+      game.checkAnswer();
+    })
+    
+    document.querySelector('#answer').addEventListener('keyup', function(event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        document.querySelector('#check-answer').click();
+      }
+    })
+    
+  }
+
+  checkAnswer() {
+      let userAnswer = parseInt(this.answerElement.value);
+      let correctAnswer = this.num1 - this.num2;
+
+      if (userAnswer === correctAnswer) {
+          this.resultElement.textContent = `You are correct!
+          You earn a character!`;
+      } else {
+          this.resultElement.textContent = `Incorrect. The correct answer is ${correctAnswer}.`;
+      }
+  }
+}
+
+
+
+document.querySelector('#add-button').addEventListener('click', function() {  
+  const game = new AdditionMathGame();
+  game.generateQuestion();
+  document.querySelector('#check-answer').addEventListener('click', function() {
+    game.checkAnswer();
+  });
+  document.querySelector('#answer').addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.querySelector('#check-answer').click();
+    }
+  })
+})
+
+document.querySelector('#subtract-button').addEventListener('click', function() {  
+  const game = new SubtractionMathGame();
+  game.generateQuestion();
+  document.querySelector('#check-answer').addEventListener('click', function() {
+    game.checkAnswer();
+  });
+  document.querySelector('#answer').addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.querySelector('#check-answer').click();
+    }
+  })
+})
+
+document.querySelector('#multiply-button').addEventListener('click', function() { 
+  const game = new MultiplicationMathGame();
+  game.generateQuestion();
+  document.querySelector('#check-answer').addEventListener('click', function() {
+    game.checkAnswer();
+  });
+  document.querySelector('#answer').addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.querySelector('#check-answer').click();
+    }
+  })
+})
+
+document.querySelector('#divide-button').addEventListener('click', function() {  
+  const game = new DivisionMathGame();
+  game.generateQuestion();
+  document.querySelector('#check-answer').addEventListener('click', function() {
+    game.checkAnswer();
+  });
+  document.querySelector('#answer').addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.querySelector('#check-answer').click();
+    }
+  })
 })
